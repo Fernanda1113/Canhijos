69ee433c7dae654825412183201b77f3ea68f09c
Fernanda1113 <erifer1113.ep@gmail.com>
2021-11-18 11:15:43 -0500

    avances

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from "./components/NavBar/NavBar"
+
+const StComponente = () => {
+  return(
+    <h1>Primer componente</h1>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar />
         <img src={logo} className="App-logo" alt="logo" />
+        <StComponente />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenidos CanHijos
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
